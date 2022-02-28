1f3b8b973d138c76b4c38447a1c30184dacea7b1
vignesh <vignesh-p@outlook.com>
2022-02-28 19:05:25 +0530

    cmt

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: 'peer-share',
+  base: '/peer-share/',
   build: {
     outDir: 'docs',
   },
